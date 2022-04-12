9722a099b95b25843d3a71fdd1a782153127f8d2
gamalielara <ara.gamaliel@ui.ac.id>
2022-04-12 20:53:05 +0700

    implemented css grid and flexbox

--- src/components/SongsLists/SongsLists.jsx
+++ src/components/SongsLists/SongsLists.jsx
@@ -1,30 +1,20 @@
-import RowItem from "../RowItem/RowItem";
+import Song from "../Song/Song";
+import "./songlists.css";
 
 const SongsLists = ({ songs, selectedSongs, setSelectedSongs }) => {
   return (
-    <table cellPadding="5">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>TITLE</th>
-          <th>ALBUM</th>
-          <th>RELEASE DATE</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody id="tbody">
-        {songs.map((song, i) => (
-          <RowItem
-            key={song.id}
-            song={song}
-            index={i + 1}
-            selectedSongs={selectedSongs}
-            setSelectedSongs={setSelectedSongs}
-            isSelected={selectedSongs && selectedSongs.includes(song.uri)}
-          />
-        ))}
-      </tbody>
-    </table>
+    <section className="songs-container">
+      {songs.map((song, i) => (
+        <Song
+          key={song.id}
+          song={song}
+          index={i + 1}
+          selectedSongs={selectedSongs}
+          setSelectedSongs={setSelectedSongs}
+          isSelected={selectedSongs && selectedSongs.includes(song.uri)}
+        />
+      ))}
+    </section>
   );
 };
 
